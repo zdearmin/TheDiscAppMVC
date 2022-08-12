a0d909f850e851b3eeef4ae61b8446ebe2a19f33
Zach Dearmin <zacharydearmin@gmail.com>
2022-08-12 11:12:32 -0400

    Player models added

---
+++ TheDiscAppMVC/Models/Player/PlayerCreate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheDiscAppMVC.Models.Player
+{
+    public class PlayerCreate
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+        public int PdgaNumber { get; set; }
+        public int PdgaRating { get; set; }
+    }
+}
