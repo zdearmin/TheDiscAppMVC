b141051348b28fea61ef7bbbdd2c33c4c159fd9f
Zach Dearmin <zacharydearmin@gmail.com>
2022-08-15 09:08:18 -0400

    Added player services with full CRUD

--- TheDiscAppMVC/Services/Player/IPlayerService.cs
+++ TheDiscAppMVC/Services/Player/IPlayerService.cs
@@ -1,6 +1,13 @@
-﻿namespace TheDiscAppMVC.Services.Player
+﻿using TheDiscAppMVC.Models.Player;
+
+namespace TheDiscAppMVC.Services.Player
 {
     public interface IPlayerService
     {
+        Task<bool> CreatePlayer(PlayerCreate model);
+        Task<IEnumerable<PlayerListItem>> GetAllPlayers();
+        Task<PlayerDetail> GetPlayerById(int id);
+        Task<bool> UpdatePlayer(PlayerEdit model);
+        Task<bool> DeletePlayer(int id);
     }
 }
--- TheDiscAppMVC/Services/Player/PlayerService.cs
+++ TheDiscAppMVC/Services/Player/PlayerService.cs
@@ -1,6 +1,54 @@
-﻿namespace TheDiscAppMVC.Services.Player
+﻿using TheDiscAppMVC.Data;
+using TheDiscAppMVC.Models.Player;
+
+namespace TheDiscAppMVC.Services.Player
 {
     public class PlayerService
     {
+        private readonly ApplicationDbContext _dbContext;
+        public PlayerService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> CreatePlayer(PlayerCreate model)
+        {
+            if (model == null)
+            {
+                return false;
+            }
+
+            _dbContext.Players.Add(new Data.Player
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+            });
+
+            if (await _dbContext.SaveChangesAsync() == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<PlayerDetail> GetPlayerById(int id)
+        {
+            var player = await _dbContext.Players.FindAsync(id);
+
+            if (player is null)
+            {
+                return null;
+            }
+
+            return new PlayerDetail
+            {
+                Id = player.Id,
+                Name = player.FirstName + " " + player.LastName,
+                PdgaNumber = (int)player.PdgaNumber,
+                PdgaRating = (int)player.PdgaRating,
+                MemberSince = (DateTime)player.MemberSince,
+            }
+        }
     }
 }
