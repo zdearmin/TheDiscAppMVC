f22e5c63ec739acc0d4d4c3627a7d408e3148627
Zach Dearmin <zacharydearmin@gmail.com>
2022-08-15 10:34:39 -0400

    Added player controller with full CRUD

---
+++ TheDiscAppMVC/Controllers/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheDiscAppMVC.Models.Player;
+using TheDiscAppMVC.Services.Player;
+
+namespace TheDiscAppMVC.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly IPlayerService _playerService;
+        public PlayerController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var players = await _playerService.GetAllPlayers();
+            return View(players);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var player = await _playerService.GetPlayerById(id);
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PlayerCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMsg"] = "Model State is Invalid";
+                return View(ModelState);
+            }
+
+            bool wasCreated = await _playerService.CreatePlayer(model);
+
+            if (wasCreated)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["ErrorMsg"] = "Unable to save to the database. Please try again later.";
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            PlayerDetail player = await _playerService.GetPlayerById(id);
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            var playerEdit = new PlayerEdit
+            {
+                Id = player.Id,
+                FirstName = player.Name
+            };
+
+            return View(playerEdit);
+        }
+    }
+}
