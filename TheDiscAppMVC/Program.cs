cd2372663b560d9d21df680b040915445d85d312
Zach Dearmin <zacharydearmin@gmail.com>
2022-08-15 13:49:48 -0400

    Working on errors with login attempts

--- TheDiscAppMVC/Program.cs
+++ TheDiscAppMVC/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TheDiscAppMVC.Data;
+using TheDiscAppMVC.Services.Collection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddScoped<ICollectionService, CollectionService>();
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
