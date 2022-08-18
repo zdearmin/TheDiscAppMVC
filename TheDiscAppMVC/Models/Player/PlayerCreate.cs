fb2213840c3b87a3366244de10619ee2f0d09871
Zach Dearmin <zacharydearmin@gmail.com>
2022-08-18 10:49:39 -0400

    Updated player model display names in view and added team dropdown list on player edit view

--- TheDiscAppMVC/Models/Player/PlayerCreate.cs
+++ TheDiscAppMVC/Models/Player/PlayerCreate.cs
@@ -6,9 +6,16 @@ namespace TheDiscAppMVC.Models.Player
     public class PlayerCreate
     {
         [StringLength(50, MinimumLength = 2)]
+        [Display(Name = "Name (Required)")]
         public string Name { get; set; }
+
+        [Display(Name = "PDGA Number")]
         public int PdgaNumber { get; set; }
+
+        [Display(Name = "PDGA Rating")]
         public int PdgaRating { get; set; }
+
+        [Display(Name = "Team")]
         public int TeamId { get; set; }
 
         public IEnumerable<SelectListItem> TeamOptions { get; set; } = new List<SelectListItem>();
--- TheDiscAppMVC/Models/Player/PlayerDetail.cs
+++ TheDiscAppMVC/Models/Player/PlayerDetail.cs
@@ -1,4 +1,5 @@
-﻿using TheDiscAppMVC.Models.Collection;
+﻿using System.ComponentModel.DataAnnotations;
+using TheDiscAppMVC.Models.Collection;
 using TheDiscAppMVC.Models.Team;
 
 namespace TheDiscAppMVC.Models.Player
@@ -7,9 +8,15 @@ namespace TheDiscAppMVC.Models.Player
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Display(Name = "PDGA Number")]
         public int PdgaNumber { get; set; }
+
+        [Display(Name = "PDGA Rating")]
         public int PdgaRating { get; set; }
         public int TeamId { get; set; }
+
+        [Display(Name = "Team")]
         public string TeamName { get; set; }
         public List<CollectionListItem> Collections { get; set; }
     }
