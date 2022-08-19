e9cae0adefadf94289a386fe6a1fb5cc1757c406
Zach Dearmin <zacharydearmin@gmail.com>
2022-08-19 12:50:52 -0400

    Added Number Of Players and Discs to Collection Models

--- TheDiscAppMVC/Models/Collection/CollectionDetail.cs
+++ TheDiscAppMVC/Models/Collection/CollectionDetail.cs
@@ -1,5 +1,6 @@
 ﻿using TheDiscAppMVC.Models.Player;
 using TheDiscAppMVC.Models.Disc;
+using System.ComponentModel.DataAnnotations;
 
 namespace TheDiscAppMVC.Models.Collection
 {
@@ -7,8 +8,12 @@ namespace TheDiscAppMVC.Models.Collection
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Display(Name = "Number of Players")]
+        public int NumOfPlayers { get; set; }
         public int PlayerId { get; set; }
         public List<PlayerListItem> Players { get; set; }
+
         public int DiscId { get; set; }
         public List<DiscListItem> Discs { get; set; }
     }
